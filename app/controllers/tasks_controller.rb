d543756579d5b9f5cf6563d392793e672c1b99b5
adamkr <adam_krawiec93@o2.pl>
2015-12-29 11:40:51 +0000

    changing edit and new form

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,31 @@
+class TasksController < ApplicationController
+    
+    def new
+        @task = Task.new
+        render :show_form
+    end
+    
+    def create
+        @task = Task.create(task_params)
+        @tasks = Task.all
+    end
+    
+    def destroy
+        @task = Task.find(params[:id])
+        @task.destroy
+        @tasks = Task.all
+    end
+    
+    def edit
+        @task = Task.find(params[:id])
+        render :show_form
+    end
+    
+    private
+    
+    def task_params
+        params.require(:task).permit(:title,
+                                     :note,
+                                     :completed)
+    end
+end
